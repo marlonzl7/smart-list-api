41534496b72b5256958b76d687ec474fd7c9dd3c
marlonzl7 <marlondesouzasampaio@gmail.com>
2025-07-03 17:04:38 -0300

    Estrutura inicial

---
+++ src/main/java/com/smartlist/api/SmartlistApiApplication.java
@@ -0,0 +1,13 @@
+package com.smartlist.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartlistApiApplication.class, args);
+	}
+
+}
