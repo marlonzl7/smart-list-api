79523fd2222378ceab3d2fd485d4ac87e60e0d9c
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-07-05 18:24:58 -0300

    feat(user): adicionar UserController

---
+++ src/main/java/com/smartlist/api/user/UserController.java
@@ -0,0 +1,24 @@
+package com.smartlist.api.user;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody @Valid RegisterDTO dto) {
+        userService.register(dto);
+        return ResponseEntity.ok("Usuário registrado com sucesso.");
+    }
+}
