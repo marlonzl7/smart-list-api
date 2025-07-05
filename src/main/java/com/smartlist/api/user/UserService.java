7e1b9a44d018e4c3da7288819dd611858eae3ea4
marlonzl7 <marlondesouzasampaio@gmail.com>
2025-07-05 18:24:27 -0300

    feat(user): adicionar UserService e RegisterDTO

---
+++ src/main/java/com/smartlist/api/user/UserService.java
@@ -0,0 +1,47 @@
+package com.smartlist.api.user;
+
+import com.smartlist.api.exceptions.EmailAlreadyExistsException;
+import com.smartlist.api.exceptions.PhoneNumberRequiredException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void register(RegisterDTO dto) {
+        if (existsByEmail(dto.email())) {
+            throw new EmailAlreadyExistsException("001", "Email já em uso.");
+        }
+
+        if (
+            (dto.notificationPreference() == NotificationPreference.WHATSAPP ||
+             dto.notificationPreference() == NotificationPreference.BOTH) &&
+            (dto.phoneNumber() == null || dto.phoneNumber().isEmpty())
+        ) {
+            throw new PhoneNumberRequiredException("002", "Número de telefone é obrigatório para notificações via Whatsapp ou ambas.");
+        }
+
+        User user = new User();
+        user.setEmail(dto.email());
+        user.setPassword(passwordEncoder.encode(dto.password()));
+        user.setNotificationPreference(dto.notificationPreference());
+        user.setThemePreference(ThemePreference.SYSTEM);
+
+        if (dto.phoneNumber() != null && !dto.phoneNumber().isEmpty()) {
+            user.setPhoneNumber(dto.phoneNumber());
+        }
+
+        userRepository.save(user);
+    }
+
+    public boolean existsByEmail(String email) {
+        return userRepository.findByEmail(email).isPresent();
+    }
+}
