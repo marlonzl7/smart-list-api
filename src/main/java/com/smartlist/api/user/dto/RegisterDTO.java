04a973ff2c1212c97a44960fdde332dbe3e697e8
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-07-07 08:43:18 -0300

    feat(user, exceptions): validar campos do registro de usuário e melhorar tratamento de unum inválido

--- src/main/java/com/smartlist/api/user/dto/RegisterDTO.java
+++ src/main/java/com/smartlist/api/user/dto/RegisterDTO.java
@@ -1,6 +1,25 @@
 package com.smartlist.api.user.dto;
 
 import com.smartlist.api.user.enums.NotificationPreference;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
-public record RegisterDTO(String email, String password, String phoneNumber, NotificationPreference notificationPreference) {
+public record RegisterDTO(
+        @NotBlank(message = "Email é obrigatório.")
+        @Email(message = "Email inválido.")
+        String email,
+
+        @NotBlank(message = "Senha é obrigatória.")
+        String password,
+
+        @Pattern(
+                regexp = "^\\(?\\d{2}\\)?\\s?9\\d{4}-\\d{4}$",
+                message = "Número de telefone inválido. Ex: (11) 99999-9999"
+        )
+        String phoneNumber,
+
+        @NotNull(message = "Preferência de notificação é obrigatória.")
+        NotificationPreference notificationPreference) {
 }
--- src/main/java/com/smartlist/api/user/enums/NotificationPreference.java
+++ src/main/java/com/smartlist/api/user/enums/NotificationPreference.java
@@ -1,7 +1,16 @@
 package com.smartlist.api.user.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum NotificationPreference {
-    EMAIL,
-    WHATSAPP,
-    BOTH
+    EMAIL, WHATSAPP, BOTH;
+
+    @JsonCreator
+    public static NotificationPreference fromString(String value) {
+        try {
+            return NotificationPreference.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException | NullPointerException ex) {
+            throw new IllegalArgumentException("notificationPreference deve ser um dos seguintes valores: EMAIL, WHATSAPP, BOTH");
+        }
+    }
 }
