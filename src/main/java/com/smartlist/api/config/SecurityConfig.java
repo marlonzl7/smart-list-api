f81b694328d9a9a96375a63245d3cc940b22fbbd
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-07-11 20:12:16 -0300

    feat(security): adicionar classe utilitária para geração e validação de JWT

--- src/main/java/com/smartlist/api/config/SecurityConfig.java
+++ src/main/java/com/smartlist/api/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.smartlist.api.infra.security;
+package com.smartlist.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
