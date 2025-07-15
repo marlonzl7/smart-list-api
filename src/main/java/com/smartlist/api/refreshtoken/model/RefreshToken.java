7318625f78c7ed323a7bef8c504c02ce540dcf1b
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-07-15 06:36:21 -0300

    feat(refresh-token): implementar lógica de refresh token rotation

---
+++ src/main/java/com/smartlist/api/refreshtoken/model/RefreshToken.java
@@ -0,0 +1,38 @@
+package com.smartlist.api.refreshtoken.model;
+
+import com.smartlist.api.user.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "refresh_token")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+
+    @Id
+    private String refreshTokenId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(unique = true)
+    private String token;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", insertable = false, updatable = false)
+    private Instant updatedAt;
+
+    @Column(name = "expires_at", nullable = false)
+    private Instant expiresAt;
+
+    private boolean used;
+}
---
+++ src/main/java/com/smartlist/api/refreshtoken/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.smartlist.api.refreshtoken.repository;
+
+import com.smartlist.api.refreshtoken.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String token);
+}
