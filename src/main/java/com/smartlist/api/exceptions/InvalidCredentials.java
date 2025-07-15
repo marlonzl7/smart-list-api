46365812dcc5708621cbf9b2c2745d604be50f2f
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2025-07-15 06:20:56 -0300

    refactor(exceptions): renomear InvalidCredential para InvalidCredentialException apenas para manter a consistência

--- src/main/java/com/smartlist/api/exceptions/InvalidCredentials.java
+++
@@ -1,7 +0,0 @@
-package com.smartlist.api.exceptions;
-
-public class InvalidCredentials extends CustomException {
-    public InvalidCredentials(String code, String message) {
-        super(code, message);
-    }
-}
