2cf745a17a1937f0f796885508de8478a9088ca7
marlonzl7 <marlondesouzasampaio@gmail.com>
2025-07-23 09:21:16 -0300

    feat(email): Implementar integração com Resend para envio de emails

---
+++ src/main/java/com/smartlist/api/email/impl/ResendEmailSender.java
@@ -0,0 +1,39 @@
+package com.smartlist.api.email.impl;
+
+import com.resend.Resend;
+import com.resend.core.exception.ResendException;
+import com.resend.services.emails.model.CreateEmailOptions;
+import com.resend.services.emails.model.CreateEmailResponse;
+import com.smartlist.api.email.core.EmailSender;
+import com.smartlist.api.exceptions.EmailSendException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResendEmailSender implements EmailSender {
+
+    private final Resend resend;
+
+    @Value("${resend.from.email}")
+    private String from;
+
+    public ResendEmailSender(Resend resend) {
+        this.resend = resend;
+    }
+
+    @Override
+    public void send(String to, String subject, String html) {
+        CreateEmailOptions emailOptions = CreateEmailOptions.builder()
+                .from(from)
+                .to(to)
+                .subject(subject)
+                .html(html)
+                .build();
+
+        try {
+            resend.emails().send(emailOptions);
+        } catch (ResendException e) {
+            throw new EmailSendException("011", "Erro ao enviar email");
+        }
+    }
+}
