d1bd7ff07239b461a06b1361e05092346a4acfa4
marlonzl7 <marlondesouzasampaio@gmail.com>
2025-07-23 11:20:55 -0300

    feat(db): adicionar script para criação de tabela e índice necessário para funcionalidade reset password

---
+++ src/main/resources/db/scripts/tables/003_create_table_password_reset_token.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS password_reset_token (
+    password_reset_token_id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    token UUID NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expires_at TIMESTAMP NOT NULL,
+    status VARCHAR(20) NOT NULL DEFAULT 'PENDING',
+    request_ip VARCHAR(45) NOT NULL,
+    user_agent TEXT NOT NULL
+);
