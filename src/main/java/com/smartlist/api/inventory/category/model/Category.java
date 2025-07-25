159270c11561432066b986387c0b2e93f221e794
marlonzl7 <marlondesouzasampaio@gmail.com>
2025-07-25 15:46:06 -0300

    feat(inventory/category): Implementar operações de CRUD na entidade Category

---
+++ src/main/java/com/smartlist/api/inventory/category/model/Category.java
@@ -0,0 +1,38 @@
+package com.smartlist.api.inventory.category.model;
+
+import com.smartlist.api.user.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "fk_user"))
+    private User user;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
