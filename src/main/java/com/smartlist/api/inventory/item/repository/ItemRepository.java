fb5cf72af3d136fe4f0cc507f65585d2c44ca537
marlonzl7 <marlondesouzasampaio@gmail.com>
2025-07-30 05:53:14 -0300

    feat(item): implementar operações de CRUD na entidade Item

---
+++ src/main/java/com/smartlist/api/inventory/item/repository/ItemRepository.java
@@ -0,0 +1,14 @@
+package com.smartlist.api.inventory.item.repository;
+
+import com.smartlist.api.inventory.item.model.Item;
+import com.smartlist.api.user.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    Optional<Item> findByUserAndItemId(User user, Long itemId);
+    Page<Item> findByUser(User user, Pageable pageable);
+}
