12e9c227828b5a2e824ac642befd2a63f891b21a
marlonzl7 <marlondesouzasampaio@gmail.com>
2025-08-02 10:15:10 -0300

    refactor(inventory/item): padronizar uso de enums nos DTOs

--- src/main/java/com/smartlist/api/inventory/item/enums/AverageConsumptionUnit.java
+++ src/main/java/com/smartlist/api/inventory/item/enums/AverageConsumptionUnit.java
@@ -1,5 +1,8 @@
 package com.smartlist.api.inventory.item.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.smartlist.api.exceptions.BadRequestException;
+
 public enum AverageConsumptionUnit {
     DAY("dia"),
     WEEK("semana"),
@@ -15,23 +18,14 @@ public enum AverageConsumptionUnit {
         return label;
     }
 
-    public static boolean isValid(String value) {
+    @JsonCreator
+    public static AverageConsumptionUnit fromString(String value) {
         for (AverageConsumptionUnit unit : AverageConsumptionUnit.values()) {
-            if (unit.getLabel().equalsIgnoreCase(value)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public static AverageConsumptionUnit fromLabel(String label) {
-        for (AverageConsumptionUnit unit : AverageConsumptionUnit.values()) {
-            if (unit.getLabel().equalsIgnoreCase(label)) {
+            if (unit.getLabel().equalsIgnoreCase(value) || unit.name().equalsIgnoreCase(value)) {
                 return unit;
             }
         }
 
-        throw new IllegalArgumentException("Unidade inválida: " + label);
+        throw new BadRequestException("EACU1001", "Unidade de medida inválida: " + value);
     }
 }
