e4fa4279c30c9a0231e4d9837a1a408d80909561
marlonzl7 <marlondesouzasampaio@gmail.com>
2026-01-27 08:24:57 -0300

    test: adicionar banco de dados em memória para o teste

--- src/test/java/com/smartlist/api/ApplicationContextTest.java
+++ src/test/java/com/smartlist/api/ApplicationContextTest.java
@@ -2,12 +2,23 @@ package com.smartlist.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-public class ApplicationContextTest {
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
+class ApplicationContextTest {
 
     @Test
     void contextLoads() {
-
+        // Teste vazio apenas para verificar se o ApplicationContext carrega
     }
 }
