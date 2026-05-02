cf1999da47c838ea248e2d74dcff1f25166008ef
marlonzl7 <98499596+marlonzl7@users.noreply.github.com>
2026-05-02 06:09:19 -0300

    feat: adiciona UserDetailsService para autenticação via email

---
+++ src/main/java/com/smartlist/api/userdetails/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.smartlist.api.userdetails;
+
+import com.smartlist.api.user.model.User;
+import com.smartlist.api.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new UserDetailsImpl(user);
+    }
+}
+
